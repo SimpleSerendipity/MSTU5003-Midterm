828835650517a5af03694615b9930a19ceef59f8
SimpleSerendipity <shiyuezheng@Shiyues-MacBook-Pro.local>
2016-11-09 18:43:43 -0500

    Initial commit adding base files

---
+++ MidtermProject.js
@@ -0,0 +1,22 @@
+var el=document.getElementById("buttonA").addEventListener("click",function(){prompt("Please enter your answer");});
+
+var learnerAnswer=prompt("Please enter your answer");
+function learnerAnswer(){
+if(learnerAnswer ==="left"){
+  alert ("Good job.");
+}
+else {
+  alert ("Sorry, you have entered the wrong answer. Please try again");
+}
+}
+
+var el=document.getElementById("buttonB").addEventListener("click",function(){prompt("Please enter your answer");});
+var learnerAnswer=prompt("Please enter your answer");
+function learnerAnswer(){
+if(learnerAnswer ==="right"){
+  alert ("Good job.");
+}
+else {
+  alert ("Sorry, you have entered the wrong answer. Please try again");
+}
+}
